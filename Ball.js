f4c0a6de32ee75e24e2eb99fb784fad7212ca9f5
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-04-25 09:02:13 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,25 +1,20 @@
-class Ball{
-constructor(x,y,r){
-
- var options={
-//density:1,
-isStatic:true,
-//frictionAir:0.4,
-   
- }
-this.ball=Bodies.circle(x,y,r,options);
-this.r=r;
-World.add(world,this.ball);
-
-}
-
-display(){
-
-ellipseMode(RADIUS);
-ellipse(this.ball.position.x,this.ball.position.y,this.r);
-
-
-}
-
-
-}
+class Ball {
+    constructor(x, y, width, height) {
+      var options = { 
+        density: 1, 
+        frictionAir: 0.005};
+  
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+  
+    display() {
+      
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      ellipse(0, 0, this.width, this.height);
+      pop();
+    }
+  }
